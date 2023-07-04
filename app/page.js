4c6ed95aeb038e9d9fac1acbb2ba5bce130c0c38
Farihul Rouf <farihulrouf@gmail.com>
2023-07-04 14:32:20 +0700

    section-2

--- app/page.js
+++ app/page.js
@@ -4,13 +4,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import NavBar from '@/components/NavBar'
 import HeroBanner from "@/components/HeroBanner"
-
+import SectionTwo from "@/components/SectionTwo"
 export default function Home() {
   return (
    <React.Fragment>
       <main className="min-h-screen py-6 bg-white">
          <NavBar />
          <HeroBanner />
+         <SectionTwo />
       </main>
    </React.Fragment>
   )
---
+++ components/SectionTwo.js
@@ -0,0 +1,20 @@
+import React from "react"
+const SectionTwo = () => {
+    return (
+        <React.Fragment>
+            <div className="max-w-[1200px] mx-auto  mt-24 mb-4 relative bg-[#F8FAFC]">
+                <div className="flex justify-center">
+                    <h2 className="text-4xl max-w-sm py-4 text-teal-700 font-semibold text-center mt-16">
+                        Smart HR to outsmart the changing world
+                    </h2>
+                </div>
+                <div className="flex justify-center">
+                    <p className="max-w-3xl py-2 text-center text-lg">The world has changed, and its going to keep changing. Dreambill helps your teams to adapt,
+                        evolve, and scale by working more effectively. Spend less time on mundane tasks and focus more
+                        on strategy. Turn data into smarter decisions and create experiences your employees will love.</p>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+export default SectionTwo
